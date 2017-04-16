0da932234bff46f06c4220a412f3e1c4baa46892
Bram <BramRausch@users.noreply.github.com>
2017-04-16 15:16:08 +0200

    Update keyPrinter.py

--- keyPrinter.py
+++ keyPrinter.py
@@ -1,22 +1,20 @@
-# Copyright 2014 Bram Rausch -- GPLv2, See license for reuse detials
 import win32com.client
 import serial
 
-ser = serial.Serial("COM3", 9600) #open the seri al port
+ser = serial.Serial("COM3", 9600) #open the serial port
 keyb = win32com.client.Dispatch("WScript.shell")
 
 while True:
 	letter = ser.read() #read the serial input from the configured port
 	
 	try: #try to convert var letter to ASCII
-		letter.decode("ASCII") 
+		letter = letter.decode("ASCII")
 	except UnicodeError: #if there is a error pass
 		pass
 	else:
-		letter = letter.decode("ASCII") #if converting the input letter does work convert it
 		letter.replace("\r", "")
-		if letter != '\x08' and letter != '\x00': #some exeptions
-			if letter == chr(8): #untested is taken from the linux keyhandler
-				keyb.sendKeys({BS}) #send keystroke
+		if letter != '\x08' and letter != '\x00':
+			if letter == chr(8): 
+				keyb.sendKeys({BS}) #send backspace
 			else:
 				keyb.sendKeys(letter) #send keystroke
